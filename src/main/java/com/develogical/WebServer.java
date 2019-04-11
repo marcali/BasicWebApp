2fab75ed2452a0e0ee42e185538f9a8036612892
marcali <alina_marchenko@outlook.com>
2019-04-11 12:02:28 +0100

    testing a build

--- src/main/java/com/develogical/WebServer.java
+++ src/main/java/com/develogical/WebServer.java
@@ -13,7 +13,7 @@ import org.eclipse.jetty.servlet.ServletHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 
 public class WebServer {
-  //build test
+  //build
 
   public WebServer() throws Exception {
 
